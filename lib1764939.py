e436abe7c829a11f19a8c36616c578ba3722afe9
Andrea Formichetti <formich93@gmail.com>
2017-02-06 14:18:21 +0100

    Import dataset and remove feature with too many nan

--- lib1764939.py
+++ lib1764939.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-# =======================================================================
+# =======================================================================================
 #   Very simple function that take a pandas' dataframe and remove
-#   any columns that exceed a certain ratio of NaN among its entries.
-# =======================================================================
+#   any columns that exceed a certain ratio(default 1/2) of NaN among its entries.
+# =======================================================================================
 def remove_nan(dataframe, threshold=1/2):
     size = len(dataframe)
     dataframe.dropna(thresh=size - np.floor(size*threshold), axis=1, inplace=True)
