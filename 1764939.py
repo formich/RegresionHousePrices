d461956299cbfdd999d0f88f8d4ac051d98ce57f
Andrea Formichetti <formich93@gmail.com>
2017-02-07 19:09:02 +0100

    Updated to Ridge Regression model from sklearn

--- 1764939.py
+++ 1764939.py
@@ -1,28 +1,67 @@
 import lib1764939 as lib
 import pandas as pd
 import numpy as np
-from sklearn import linear_model
+from sklearn.linear_model import Ridge
 from matplotlib import pyplot as plt
 
 
-# =======================================================================
-#    Importing dataset into pandas' DataFrame
-# =======================================================================
+# Importing dataset into pandas' DataFrame
+
 train = pd.read_csv("data/train.csv", index_col=0)
 test = pd.read_csv("data/test.csv", index_col=0)
-whole_set = pd.concat((train.loc[:, 'MSSubClass':'SaleCondition'], test.loc[:, 'MSSubClass':'SaleCondition']))
+whole_set = pd.concat((train.loc[:, 'MSSubClass':'SaleCondition'],
+                       test.loc[:, 'MSSubClass':'SaleCondition']))
 
-# =======================================================================
-#   Remove all features that contains too many NaN, if we don't have
-#   enough information about some feature it could ruin the prediction
-# =======================================================================
+#  Remove all features that contains too many NaN
+
 lib.remove_nan(whole_set, threshold=0.4)
 
+# Since Kaggle evaluates submissions using the logarithm of the predicted value
+# makes sense to train the model with the logarithm of the prices
+
+train["SalePrice"] = np.log(train["SalePrice"])
+
+# Some features in the dataset are converted to numerical version
+
+whole_set = lib.bulk_convert_cat_to_num(whole_set)
 
 
+# Extract numerical features and remove from the list features that
+# are no apparent highly price-related
+
+whole_set = whole_set.drop(["LotFrontage", "LandSlope", "LotShape", "LotConfig",
+                            "Neighborhood", "MSSubClass", "MasVnrType", "MasVnrArea"], axis=1)
+numeric_features = whole_set.dtypes[whole_set.dtypes != "object"].index
+
+# Here we are computing the skewness of the samples, if it's bigger than a specific
+# threshold I will transform the features by applying log(feature + 1),
+# this will make the features more "Normal"
+
+skewness_arr = lib.compute_skewness(train[numeric_features])
+skewed_features = lib.select_skewed_features(skewness_arr)
+whole_set[skewed_features] = np.log1p(whole_set[skewed_features])
+
+# Generate DummyVariable from the remaining categorical variable
+whole_set = pd.get_dummies(whole_set)
+
+# Filling NaN values with the mean of the column
+whole_set = whole_set.fillna(whole_set.mean())
+
+print("Preprocessing terminate")
+
+# Creating matrices and prediction vector to generate model
+X = whole_set[:train.shape[0]]
+y = train.SalePrice
+X_test = whole_set[train.shape[0]:]
+
+opt_alpha = lib.cv(X, y, test_range=100)
+
+print("optimal alpha computed:", opt_alpha)
 
 
+ridge = Ridge(alpha=opt_alpha, fit_intercept=True)
+ridge.fit(X, y)
 
-
-
-
+# --------------------- Prediction
+pred = pd.DataFrame({"SalePrice": np.exp(ridge.predict(X_test))}, index=test.index)
+pred.to_csv("pred.csv")
--- lib1764939.py
+++ lib1764939.py
@@ -1,13 +1,85 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from scipy.stats import skew
+from sklearn.linear_model import Ridge, RidgeCV
+from sklearn import cross_validation
 
 
-# =======================================================================================
+
+# =======================================================================================================
 #   Very simple function that take a pandas' dataframe and remove
 #   any columns that exceed a certain ratio(default 1/2) of NaN among its entries.
-# =======================================================================================
+# =======================================================================================================
 def remove_nan(dataframe, threshold=1/2):
     size = len(dataframe)
     dataframe.dropna(thresh=size - np.floor(size*threshold), axis=1, inplace=True)
 
 
+# =======================================================================================================
+#   function compute_skewness take a pandas' dataframe and computes skewness for each columns,
+#   return a pandas' Series containing the skewness indexed with columns names.
+# =======================================================================================================
+def compute_skewness(dataframe):
+    skewness = []
+    for column in dataframe:
+        skewness.append(skew(dataframe[column].dropna()))
+    s = pd.Series(skewness, index=dataframe.columns)
+    return s
+
+
+# =======================================================================================================
+#   function select_skewed_features take a pandas' dataframe and computes skewness for each columns,
+#   return a pandas' Series containing the skewness indexed with columns names.
+# =======================================================================================================
+def select_skewed_features(series, skew_thresh=0.75):
+    skewed = []
+    for col in series.index:
+        if series[col] > skew_thresh:
+            skewed.append(col)
+    return skewed
+
+
+# =======================================================================================================
+#   Like its name suggests this utility function convert categorical feature into a numerical version.
+#   Requires a target dataframe, the feature that has to be converted and a list of possible values that
+#   the feature can take. Return a new dataframe with requested variable converted
+# =======================================================================================================
+def convert_feature_cat_to_num(dataframe, feature, value_list):
+    for index, row in dataframe.iterrows():
+        if dataframe.loc[index, feature] not in value_list:
+            dataframe.loc[index, feature] = 0
+        else:
+            dataframe.loc[index, feature] = value_list.index(row[feature]) + 1
+    return dataframe
+
+
+def bulk_convert_cat_to_num(dataframe):
+    dataframe = convert_feature_cat_to_num(dataframe, "ExterQual", ["Po", "Fa", "TA", "Gd", "Ex"])
+    dataframe = convert_feature_cat_to_num(dataframe, "Street", ["Grvl", "Pave"])
+    dataframe = convert_feature_cat_to_num(dataframe, "LandContour", ["Low", "HLS", "Bnk", "Lvl"])
+    dataframe = convert_feature_cat_to_num(dataframe, "Utilities", ["ELO", "NoSeWa", "NoSewr", "AllPub"])
+    dataframe = convert_feature_cat_to_num(dataframe, "ExterCond", ["Po", "Fa", "TA", "Gd", "Ex"])
+    dataframe = convert_feature_cat_to_num(dataframe, "BsmtQual", ["Po", "Fa", "TA", "Gd", "Ex"])
+    dataframe = convert_feature_cat_to_num(dataframe, "BsmtCond", ["Po", "Fa", "TA", "Gd", "Ex"])
+    dataframe = convert_feature_cat_to_num(dataframe, "BsmtExposure", ["Mn", "Av", "Gd"])
+    dataframe = convert_feature_cat_to_num(dataframe, "BsmtFinType1", ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"])
+    dataframe = convert_feature_cat_to_num(dataframe, "BsmtFinType2", ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"])
+    dataframe = convert_feature_cat_to_num(dataframe, "HeatingQC", ["Po", "Fa", "TA", "Gd", "Ex"])
+    dataframe = convert_feature_cat_to_num(dataframe, "CentralAir", ["No", "Yes"])
+    dataframe = convert_feature_cat_to_num(dataframe, "KitchenQual", ["Po", "Fa", "TA", "Gd", "Ex"])
+    dataframe = convert_feature_cat_to_num(dataframe, "Functional", ["Sal", "Sev", "Maj2", "Maj1",
+                                                                     "Mod", "Min2", "Min1", "Typ"])
+    dataframe = convert_feature_cat_to_num(dataframe, "GarageFinish", ["Unf", "RFn", "Fin"])
+    dataframe = convert_feature_cat_to_num(dataframe, "GarageQual", ["Po", "Fa", "TA", "Gd", "Ex"])
+    dataframe = convert_feature_cat_to_num(dataframe, "GarageCond", ["Po", "Fa", "TA", "Gd", "Ex"])
+    dataframe = convert_feature_cat_to_num(dataframe, "PavedDrive", ["N", "P", "Y"])
+    return dataframe
+
+
+def cv(X, y, test_range=100):
+    preds = []
+    for alpha in np.arange(0, test_range):
+        ridge_cv = Ridge(alpha).fit(X, y)
+        preds.append(cross_validation.cross_val_score(ridge_cv, X, y, cv=10).mean())
+    opt_alpha = np.argmax(preds)+1
+    return opt_alpha
